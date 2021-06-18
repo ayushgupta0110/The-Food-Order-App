34459ebccbaefdf1c5075cbc95975afa816c86c7
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2021-06-18 18:03:23 +0530

    minor changes

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,6 @@
 import React from "react";
 import classes from "./Cart.module.css";
+import Modal from '../UI/Modal';
 
 const Cart = () => {
   const cartitems = (
@@ -13,18 +14,17 @@ const Cart = () => {
   );
 
   return (
-    <div>
+    <Modal>
       {cartitems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>35.62</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["button--alt"]}></button>
+        <button className={classes["button--alt"]}>Cancel</button>
         <button className={classes.button}>Order</button>
       </div>
-      <div></div>
-    </div>
+    </Modal>
   );
 };
 
