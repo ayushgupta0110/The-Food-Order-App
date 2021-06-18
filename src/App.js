4b1a871ad521a3e633b9459807c4b97d7b996c90
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2021-06-18 18:32:34 +0530

    Added Modal State and managing Cart

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React,{Fragment} from 'react';
-import Cart from './components/Cart/Cart';
-import Header from './components/Layouts/Header';
-import Meals from './components/Meals/Meals';
+import React, { Fragment, useState } from "react";
+import Cart from "./components/Cart/Cart";
+import Header from "./components/Layouts/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
+  const [isCartShown,setIsCartShown] = useState(false);
+
+  const showCartHandler = () => {
+    setIsCartShown(true);
+  }
+
+  const hideCartHandler = () => {
+    setIsCartShown(false);
+  }
+
   return (
     <Fragment>
-    <Cart />
-    <Header/>
-    <main>
-      <Meals />
-    </main>
+      {isCartShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,7 +2,7 @@ import React from "react";
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
 
-const Cart = () => {
+const Cart = (props) => {
   const cartitems = (
     <ul className={classes["cart-items"]}>
       {[{ id: "c1", name: "sushi", amount: "3", price: "12.99" }].map(
@@ -14,14 +14,14 @@ const Cart = () => {
   );
 
   return (
-    <Modal>
+    <Modal onClose={props.onClose}>
       {cartitems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>35.62</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["button--alt"]}>Cancel</button>
+        <button className={classes["button--alt"]} onClick={props.onClose}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
     </Modal>
