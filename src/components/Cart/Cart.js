e8a6612e21c9224b2991c1c885e053aee139a986
Ayush Gupta <76724104+ayushgupta0110@users.noreply.github.com>
2021-06-19 18:20:54 +0530

    Added logic to output Cart Items

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useContext } from "react";
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
+import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const onRemoveHandler = id => {
+
+  }
+
+  const onAddHandler = item => {
+
+  }
+
   const cartitems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "c1", name: "sushi", amount: "3", price: "12.99" }].map(
-        (item) => (
-          <li>{item.name}</li>
-        )
-      )}
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={onRemoveHandler.bind(null,item.id)}
+          onAdd={onAddHandler.bind(null,item)}
+        />
+      ))}
     </ul>
   );
 
@@ -18,11 +38,13 @@ const Cart = (props) => {
       {cartitems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["button--alt"]} onClick={props.onClose}>Close</button>
-        <button className={classes.button}>Order</button>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
